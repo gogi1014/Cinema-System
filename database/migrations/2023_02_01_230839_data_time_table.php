fb78178e3a6774139be9cba0eaef35f60a4b5199
gogi1014 <gogi1014@abv.bg>
2023-02-02 01:11:09 +0200

    tAd-migration

---
+++ database/migrations/2023_02_01_230839_data_time_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('moviedates', function (Blueprint $table) {
+            $table->id();
+            $table->integer('MovieId');
+            $table->string('MovieName');
+            $table->date('date');
+            $table->string('time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
